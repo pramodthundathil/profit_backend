f603cd31089abf77b98a9b783e6b9b8589452763
pramod <gopinath.pramod@gmail.com>
2026-04-29 15:11:03 +0530

    subscription plans

--- member_api/serializers.py
+++ member_api/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework import serializers
 from members.models import Member, Subscription, SubscriptionInstallment
+from payments.models import Payment
 from home.models import CustomUser
 
 class SendOTPSerializer(serializers.Serializer):
@@ -22,6 +22,14 @@ class SetPasswordSerializer(serializers.Serializer):
             raise serializers.ValidationError("Passwords do not match.")
         return data
 
+class PaymentHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = [
+            'id', 'receipt_number', 'amount', 'payment_date', 
+            'payment_method', 'status', 'is_installment', 'installment_number'
+        ]
+
 class SubscriptionInstallmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubscriptionInstallment
@@ -29,6 +37,7 @@ class SubscriptionInstallmentSerializer(serializers.ModelSerializer):
 
 class SubscriptionSerializer(serializers.ModelSerializer):
     type_name = serializers.CharField(source='subscription_type.name', read_only=True, default="Custom")
+    batch_name = serializers.CharField(source='batch.name', read_only=True, allow_null=True)
     installments = SubscriptionInstallmentSerializer(many=True, read_only=True)
     
     class Meta:
@@ -36,13 +45,17 @@ class SubscriptionSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'type_name', 'duration', 'duration_unit', 
             'start_date', 'end_date', 'final_amount', 'amount_paid', 
-            'balance_amount', 'status', 'is_fully_paid', 'installments'
+            'balance_amount', 'status', 'is_fully_paid', 'installments',
+            'batch_name', 'includes_personal_training', 'personal_training_sessions',
+            'includes_diet_plan', 'includes_locker', 'locker_number',
+            'freeze_allowed', 'freeze_days_allowed', 'freeze_days_used'
         ]
 
 class MemberProfileSerializer(serializers.ModelSerializer):
     gym_name = serializers.CharField(source='gym.name', read_only=True)
     branch_name = serializers.CharField(source='branch.name', read_only=True, allow_null=True)
     subscriptions = SubscriptionSerializer(many=True, read_only=True)
+    payments = PaymentHistorySerializer(many=True, read_only=True)
 
     class Meta:
         model = Member
@@ -50,5 +63,6 @@ class MemberProfileSerializer(serializers.ModelSerializer):
             'id', 'member_id', 'first_name', 'last_name', 'email', 'mobile_number',
             'gender', 'date_of_birth', 'height', 'weight', 'blood_group',
             'membership_status', 'access_enabled', 'access_expiry_date',
-            'gym_name', 'branch_name', 'photo', 'subscriptions'
+            'access_token', 'gym_name', 'branch_name', 'photo', 
+            'subscriptions', 'payments'
         ]
